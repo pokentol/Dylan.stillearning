04d997e0869c35fdb943fabf8fd5100a5c418fd7
pokentol <129358852+pokentol@users.noreply.github.com>
2023-09-02 10:42:46 +0700

    first commit

---
+++ apalah.py
@@ -0,0 +1,27 @@
+# apel = 48.8
+# print(apel)
+# print(type(apel))
+# apel2 = 52
+# nama_apel =
+# list_wana_apel = ['hijau', 'merah', 'kuning']
+# list_apel1 = ['manis', 2 , 3.0]
+# print(list_wana_apel)
+# print(list_apel1)
+
+# tuple_jumlah = (2, 5.55, "satu")
+# print (tuple_jumlah)
+
+# apel_dictonary = {'nama':'tobrut' 'asal':'NGAWI','ukuran':'G'}
+# print(apel_dictonary['nama'])
+# print(apel_dictonary['asal'])
+# print(apel_dictonary['ukuran'])
+
+# print(45+5)
+# print(55-5)
+# print(10*5)
+# print(50/5)
+
+a = 51
+b = 5
+print(a // b)
+
